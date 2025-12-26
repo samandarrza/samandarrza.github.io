f500630cc46a44bec2c7a78465f37d62f7ec73b6
Samandar Rzayev <110488821+samandarrza@users.noreply.github.com>
2025-12-26 12:14:12 +0400

    Update price.js

--- js/price.js
+++ js/price.js
@@ -43,36 +43,36 @@ function calculateResult(turnover, serviceType, taxpayer, employees, vatPayer, f
     var result = 0;
 
     if (serviceType === 'micro') {
-        result += 50;
-        result += Math.max(110, Math.floor(turnover / 90000) * 10);
+        result += 40;
+        result += Math.max(55, Math.floor(turnover / 18000) * 10);
     }
 
     if (serviceType === 'small') {
-        result += 100;
+        result += 90;
         if (turnover>1000000) {
-            result += Math.max(700, Math.floor(turnover / 230000) * 10);
+            result += Math.max(350, Math.floor(turnover / 46000) * 10);
         }
         else if (turnover>1500000) {
-            result += Math.max(1000, Math.floor(turnover / 300000) * 10);
+            result += Math.max(500, Math.floor(turnover / 60000) * 10);
         }
         else{
-            result += Math.max(155, Math.floor(turnover / 140000) * 10);
+            result += Math.max(80, Math.floor(turnover / 28000) * 10);
         }
     }
 
     result += Math.max(20, employees * 10);
 
     if (taxpayer === 'simplified') {
-        result += 70;
+        result += 50;
     } else if (taxpayer === 'profit' || taxpayer === 'income') {
-        result += 90;
+        result += 70;
     }
 
     if (vatPayer === 'yes') {
-        result += Math.round(100 + turnover / 10000);
+        result += Math.round(70 + turnover / 10000);
     }
     if (fixedAssets === 'yes') {
-        result += Math.round(100 + turnover / 10000);
+        result += Math.round(70 + turnover / 10000);
     }
 
     document.getElementById('result').innerText = 'Ödəniləcək məbləğ: ' + result + ' AZN';
